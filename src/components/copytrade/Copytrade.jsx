43f06cdbec1ffce9cf912f13b04c39e6ff5dbfdb
trilvid <kelvinogodo45@gmail.com>
2025-08-17 13:24:34 +0100

    working on homepage

--- src/components/copytrade/Copytrade.jsx
+++ src/components/copytrade/Copytrade.jsx
@@ -18,7 +18,7 @@ const Copytrade = () => {
                   </p>
         </div>
         <div className="about-page-img forex-img-container">
-            <img src="/apexmockup6.png" className='forex-img '/>
+            <img src="/copixmockup2.png" className='forex-img '/>
             </div>
         </div>
     </div>
